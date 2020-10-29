9df7bddcd2057a24b57185dff2121d2afa424e2b
Taif Rahaman <taif.rahaman73@gmail.com>
2020-10-30 03:04:46 +0600

    hero section added

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElement";
+
+import { Button } from "../ButtonElement";
+import Video from "../../videos/video.mp4";
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Ut tempus malesuada arcu a consectetur. Suspendisse sapien ex,
+          elementum quis dolor ut, imperdiet molestie leo. Donec ut commodo
+          quam.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
