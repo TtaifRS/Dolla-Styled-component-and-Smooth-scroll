0aaed45b185b0dde6c6abff787060400e36694e1
Taif Rahaman <taif.rahaman73@gmail.com>
2020-10-30 20:51:57 +0600

    info section added

---
+++ src/components/Info/Data.js
@@ -0,0 +1,57 @@
+import svg1 from "../../images/svg-1.svg";
+import svg2 from "../../images/svg-2.svg";
+import svg3 from "../../images/svg-3.svg";
+
+export const homeObject = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transactions with zero fees",
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia esse rem atque iste sunt non ducimus labore nisi quo a quos minima voluptatem praesentium beatae animi deserunt neque, voluptate explicabo.",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: svg1,
+  alt: "car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
+
+export const homeObjectTwo = {
+  id: "discover",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Unlimited Access",
+  headline: "Login to your account any time",
+  description:
+    "Officia esse rem atque iste sunt non ducimus labore nisi quo a quos minima voluptatem praesentium beatae animi deserunt neque, voluptate explicabo.",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: svg2,
+  alt: "piggybank",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+
+export const homeObjectThree = {
+  id: "signup",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Join our team",
+  headline: "Creating an account is extremely easy",
+  description:
+    "Officia esse rem atque iste sunt non ducimus labore nisi quo a quos minima voluptatem praesentium beatae animi deserunt neque, voluptate explicabo.",
+  buttonLabel: "Start Now",
+  imgStart: false,
+  img: svg3,
+  alt: "account",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
---
+++ src/components/Info/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  InfoContainer,
+  InfoWrapper,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  InfoRow,
+  BtnWrap,
+  ImgWrap,
+  Img,
+} from "./InfoElement";
+
+import { Button } from "../ButtonElement";
+
+const InfoSection = ({
+  lightBg,
+  id,
+  imgStart,
+  topLine,
+  lightText,
+  headline,
+  darkText,
+  description,
+  buttonLabel,
+  alt,
+  primary,
+  dark,
+  dark2,
+  img,
+}) => {
+  return (
+    <InfoContainer lightBg={lightBg} id={id}>
+      <InfoWrapper>
+        <InfoRow imgStart={imgStart}>
+          <Column1>
+            <TextWrapper>
+              <TopLine>{topLine}</TopLine>
+              <Heading lightText={lightText}>{headline}</Heading>
+              <Subtitle darkText={darkText}>{description}</Subtitle>
+              <BtnWrap>
+                <Button
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  primary={primary ? 1 : 0}
+                  dark={dark ? 1 : 0}
+                  dark2={dark2 ? 1 : 0}
+                  to="home"
+                >
+                  {buttonLabel}
+                </Button>
+              </BtnWrap>
+            </TextWrapper>
+          </Column1>
+          <Column2>
+            <ImgWrap>
+              <Img src={img} alt={alt} />
+            </ImgWrap>
+          </Column2>
+        </InfoRow>
+      </InfoWrapper>
+    </InfoContainer>
+  );
+};
+
+export default InfoSection;
